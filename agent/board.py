aa97e649fb7116434685076f697444d7315c9927
Kexin (Clare) Chen <108605930+kexincchen@users.noreply.github.com>
2023-04-18 20:35:51 +1000

    implemented render() in Board

--- agent/board.py
+++ agent/board.py
@@ -2,8 +2,8 @@ from referee.game import \
     PlayerColor, Action, SpawnAction, SpreadAction, HexPos, HexDir
     
 MAX_TURNS = 7 * 7 * 7
-
-class Board:
+BOARD_N = 7
+class MatrixBoard:
     def __init__(self, state) -> None:
         # create an empty board with each cell representing the power of player
         # channel 1: red 
@@ -23,7 +23,8 @@ class Board:
                 else:
                     self.state[1][r][q] = 1
                 pass
-            case SpreadAction(cell,direction):
+            
+            case SpreadAction(cell, direction):
                 r = cell.r
                 q = cell.q
                 dr = direction.value.r
@@ -36,6 +37,7 @@ class Board:
                     opponent = 0
                 power = self.state[curr_player][r][q]
                 self.state[curr_player][r][q] = 0 #remove the token
+                
                 #Spread
                 for i in range(power):
                     r,q = self.update_r_q(r,q,dr,dq)
@@ -59,7 +61,7 @@ class Board:
             self.blue_power == 0
         ])
     
-        # Function to update the r,q if they reach -1 or 7
+    # Function to update the r,q if they reach -1 or 7
     def update_r_q(r, q, dr, dq) -> tuple:
         """
         Update the coordinates if the token reaches the edge.
@@ -75,3 +77,42 @@ class Board:
         if q < 0:
             q = 6
         return (r, q)
+    
+    def render(self, use_color: bool=False, use_unicode: bool=False) -> str:
+        """
+        Return a visualisation of the game board via a multiline string. The
+        layout corresponds to the axial coordinate system as described in the
+        game specification document.
+        """
+        def apply_ansi(str, bold=True, color=None):
+            # Helper function to apply ANSI color codes
+            bold_code = "\033[1m" if bold else ""
+            color_code = ""
+            if color == "r":
+                color_code = "\033[31m"
+            if color == "b":
+                color_code = "\033[34m"
+            return f"{bold_code}{color_code}{str}\033[0m"
+
+        dim = BOARD_N
+        output = ""
+        for row in range(dim * 2 - 1):
+            output += "    " * abs((dim - 1) - row)
+            for col in range(dim - abs(row - (dim - 1))):
+                # Map row, col to r, q
+                r = max((dim - 1) - row, 0) + col
+                q = max(row - (dim - 1), 0) + col
+                
+                if (self.state[0, r, q] != 0) or (self.state[1, r, q] != 0):
+                    power = self.state[0, r, q]
+                    color = "r" if self.state[0, r, q] != 0 else "b"
+                    text = f"{color}{power}".center(4)
+                    if use_color:
+                        output += apply_ansi(text, color=color, bold=False)
+                    else:
+                        output += text
+                else:
+                    output += " .. "
+                output += "    "
+            output += "\n"
+        return output
--- agent/program.py
+++ agent/program.py
@@ -1,6 +1,6 @@
 # COMP30024 Artificial Intelligence, Semester 1 2023
 # Project Part B: Game Playing Agent
-from agent import Board
+from agent.board import MatrixBoard
 import numpy as np
 from referee.game import \
     PlayerColor, Action, SpawnAction, SpreadAction, HexPos, HexDir
@@ -18,7 +18,7 @@ class Agent:
         Initialise the agent.s
         """
         self._color = color
-        board = Board(np.zeros([2,7,7]))
+        self._board = MatrixBoard(np.zeros([2,7,7], dtype=int))
         match color:
             case PlayerColor.RED:
                 print("Testing: I am playing as red")
