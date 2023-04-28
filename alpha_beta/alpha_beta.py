3beb3479251169ba44468662756522d303ba07c8
Kexin (Clare) Chen <108605930+kexincchen@users.noreply.github.com>
2023-04-28 12:29:36 +1000

    added alpha_beta module

---
+++ alpha_beta/alpha_beta.py
@@ -0,0 +1,77 @@
+from alpha_beta.board import MatrixBoard
+from referee.game import \
+    PlayerColor, Action, SpawnAction, SpreadAction, HexPos, HexDir
+import random
+class ABNode:
+    def __init__(self, board: MatrixBoard, color: PlayerColor):
+        self.board = MatrixBoard(board.state, board.turn_count, board.red_power, board.blue_power)
+        self.children = []
+        self.color = color
+        self.action = None
+        
+    def add_children(self):
+        all_children =[]
+        for action in self.board.get_valid_actions(self.color):
+            child_board= self.board.next_board(action, self.color)
+            child = ABNode(child_board, PlayerColor.RED if self.color == PlayerColor.BLUE else PlayerColor.BLUE)
+            child.action = action
+            all_children.append(child)
+        all_children.sort(key = key_function)
+        # use top K algorithm: select k nodes with highest utility
+        for child in all_children[:10]:
+            self.children.append(child)
+
+
+def alpha_beta(node: ABNode, depth: int, alpha, beta, maximize:bool, color: PlayerColor):
+    if (depth==0) or (node.board.game_over()):
+        return eval(node.board, color)
+    if (maximize==True):
+        best = float('-inf')
+        for child in node.children:
+            child.add_children()
+            child_eval = alpha_beta(child, depth-1, alpha, beta, False, color)
+            best = max(best, child_eval)
+            alpha = max(alpha, best)
+            if beta<=alpha:
+                break
+        return best
+    else:
+        worst = float('inf')
+        child_len = len(node.children)
+        for child in node.children:
+            child.add_children()
+            child_eval = alpha_beta(child, depth-1, alpha, beta, True, color)
+            worst = min(beta, child_eval)
+            beta = min(beta, worst)
+            if beta<=alpha:
+                break
+        return worst
+
+def minimax_with_alpha_beta(node: ABNode, color:PlayerColor, depth:int):
+    values = dict()
+    for child in node.children:
+        child.add_children()
+        values.update({child.action: alpha_beta(child,depth,float('-inf'),float('inf'), False, color)})
+    choices = []
+    best_eval = max(values.values())
+    for action in values.keys():
+        if values[action] == best_eval:
+            choices.append(action)
+    return random.choice(choices)
+
+def eval(board: MatrixBoard, color: PlayerColor):
+    if color == PlayerColor.RED:
+        return board.red_power-board.blue_power
+    else:
+        return board.blue_power-board.red_power
+    
+def key_function(obj):
+    util =-100
+    util = utility(obj.board, obj.color)
+    return (util, random.random())
+    
+def utility(board: MatrixBoard, color: PlayerColor):
+    if color == PlayerColor.RED:
+        return board.red_power-board.blue_power
+    else:
+        return board.blue_power-board.red_power
---
+++ alpha_beta/board.py
@@ -0,0 +1,212 @@
+from referee.game import \
+    PlayerColor, Action, SpawnAction, SpreadAction, HexPos, HexDir
+import random
+
+MAX_TURNS = 7 * 7 * 7
+BOARD_N = 7
+DRAW =0
+RED_WIN=1
+BLUE_WIN=2
+WIN_POWER_DIFF  = 2
+
+class MatrixBoard:
+    def __init__(self, state, turn_count, red_power, blue_power) -> None:
+        # create an empty board with each cell representing the power of player
+        # channel 1: red 
+        # channel 2: blue
+        self.state = state.copy()
+        self.turn_count = turn_count
+        self.red_power = red_power
+        self.blue_power = blue_power
+        
+    def apply_action(self, action: Action, color: PlayerColor):
+        
+        match action:
+            case SpawnAction(cell):
+                r = cell.r
+                q = cell.q
+                if (color == PlayerColor.RED):
+                    self.state[0][r][q] = 1
+                    self.red_power += 1
+                else:
+                    self.state[1][r][q] = 1
+                    self.blue_power += 1
+                pass
+            
+            case SpreadAction(cell, direction):
+                r = cell.r
+                q = cell.q
+                dr = direction.value.r
+                dq = direction.value.q
+                # print("r=%d, q=%d, dr=%d, dq=%d" %(r, q, dr, dq))
+                if (color == PlayerColor.RED):
+                    curr_player = 0
+                    opponent = 1
+                else:
+                    curr_player = 1
+                    opponent = 0
+                power = self.state[curr_player][r][q]
+                self.state[curr_player][r][q] = 0 #remove the token
+                
+                # Spread
+                for i in range(power):
+                    # print("r=%d, q=%d, dr=%d, dq=%d" %(r, q, dr, dq))
+                    r, q = self.update_r_q(r, q, dr, dq)
+                    self.state[curr_player][r][q] += self.state[opponent][r][q] + 1
+                    '''
+                    if curr_player eats opponent's token, the power of the current player add the power
+                    of the token eaten
+                    '''
+                    if (curr_player == 0):
+                        self.red_power += self.state[opponent][r][q]
+                        self.blue_power -= self.state[opponent][r][q]
+                    else:
+                        self.red_power -= self.state[opponent][r][q]
+                        self.blue_power += self.state[opponent][r][q]
+                    self.state[opponent][r][q] = 0
+                    '''if the token is larger than 6, reduce the power of the current player by 7
+                    and remove the token
+                    '''
+                    if self.state[curr_player][r][q] > 6:
+                        self.state[curr_player][r][q] = 0
+                        if (curr_player == 0):
+                            self.red_power -= 7
+                        else:
+                            self.blue_power -= 7
+                pass
+
+    def game_over(self) -> bool:
+        """
+        True if the game is over.
+        """
+        if self.turn_count < 2: 
+            return False
+        
+        return any([
+            self.turn_count >= MAX_TURNS,
+            self.red_power == 0,
+            self.blue_power == 0
+        ])
+    
+    def next_board(self, action: Action, playerColor: PlayerColor):
+        next = MatrixBoard(self.state, self.turn_count, self.red_power, self.blue_power)
+        next.apply_action(action, playerColor)
+        next.turn_count += 1
+        return next
+
+    
+        # Function to update the r,q if they reach -1 or 7
+    def update_r_q(self, r, q, dr, dq) -> tuple:
+        """
+        Update the coordinates if the token reaches the edge.
+        """
+        r += dr
+        q += dq
+        if r > 6:
+            r = 0
+        if r < 0:
+            r = 6
+        if q > 6:
+            q = 0
+        if q < 0:
+            q = 6
+        return (r, q)
+    
+
+    #function to get all valid actions in the current board
+    def get_valid_actions(self, color: PlayerColor):
+        output = []
+        if (color == PlayerColor.RED):
+            curr_player = 0
+            opponent = 1
+        else:
+            curr_player = 1
+            opponent = 0
+        empty = []
+        for r in range(7):
+            for q in range(7):
+                if (self.state[curr_player][r][q]==0 and (self.red_power+self.blue_power)<49):
+                    empty.append(HexPos(r, q))
+                elif (self.state[curr_player][r][q] !=0 ):
+                    output.append(SpreadAction(HexPos(r,q), HexDir.DownRight))
+                    output.append(SpreadAction(HexPos(r,q), HexDir.Down))
+                    output.append(SpreadAction(HexPos(r,q), HexDir.DownLeft))
+                    output.append(SpreadAction(HexPos(r,q), HexDir.UpLeft))
+                    output.append(SpreadAction(HexPos(r,q), HexDir.Up))
+                    output.append(SpreadAction(HexPos(r,q), HexDir.UpRight))
+        
+        for position in empty:
+            if (self.state[opponent][position.r][position.q]==0):
+                output.append(SpawnAction(position))
+        return output
+    
+    #function to simulate a game from the current state
+    def playout(self, start_color:PlayerColor) -> PlayerColor | None:
+        playout_board = MatrixBoard(self.state, self.turn_count, self.red_power, self.blue_power)
+        curr_player = start_color
+        while not (playout_board.game_over()):
+            playout_board = MatrixBoard(playout_board.state, playout_board.turn_count, playout_board.red_power, playout_board.blue_power)
+            actions = playout_board.get_valid_actions(curr_player)
+            random.seed(1000)
+            action = actions[random.randint(0, len(actions) - 1)]
+            # action = random.choice(actions)
+            playout_board.apply_action(action, curr_player)
+            playout_board.turn_count += 1
+            
+            curr_player = PlayerColor.BLUE if curr_player == PlayerColor.RED else PlayerColor.RED
+            
+        return playout_board.winner()
+    
+    def playout_heuristic(self, start_color:PlayerColor) -> PlayerColor | None:
+        result = PlayerColor.RED if self.red_power > self.blue_power else PlayerColor.BLUE
+        return result
+        
+    # function to get the winner of the board
+    def winner(self) -> PlayerColor | None:
+        if (self.red_power == 0 and self.blue_power == 0):
+            return None
+        elif (abs(self.red_power - self.blue_power) < WIN_POWER_DIFF):
+            return None
+        elif (self.red_power > self.blue_power):
+            return PlayerColor.RED
+        elif (self.red_power < self.blue_power):
+            return PlayerColor.BLUE
+
+    def render(self, use_color: bool=False, use_unicode: bool=False) -> str:
+        """
+        Return a visualisation of the game board via a multiline string. The
+        layout corresponds to the axial coordinate system as described in the
+        game specification document.
+        """
+        def apply_ansi(str, bold=True, color=None):
+            # Helper function to apply ANSI color codes
+            bold_code = "\033[1m" if bold else ""
+            color_code = ""
+            if color == "r":
+                color_code = "\033[31m"
+            if color == "b":
+                color_code = "\033[34m"
+            return f"{bold_code}{color_code}{str}\033[0m"
+
+        dim = BOARD_N
+        output = ""
+        for row in range(dim * 2 - 1):
+            output += "    " * abs((dim - 1) - row)
+            for col in range(dim - abs(row - (dim - 1))):
+                # Map row, col to r, q
+                r = max((dim - 1) - row, 0) + col
+                q = max(row - (dim - 1), 0) + col
+                
+                if (self.state[0, r, q] != 0) or (self.state[1, r, q] != 0):
+                    power = self.state[0, r, q] if self.state[0, r, q] != 0 else self.state[1, r, q] 
+                    color = "r" if self.state[0, r, q] != 0 else "b"
+                    text = f"{color}{power}".center(4)
+                    if use_color:
+                        output += apply_ansi(text, color=color, bold=False)
+                    else:
+                        output += text
+                else:
+                    output += " .. "
+                output += "    "
+            output += "\n"
+        return output
